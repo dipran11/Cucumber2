1801f1ec7d224fdfef2e5c5d22c5f746ba20cf93
dipran11 <158012785+dipran11@users.noreply.github.com>
2024-09-20 11:33:55 +0700

    Update Hooks.java

--- src/test/java/stepDef/Hooks.java
+++ src/test/java/stepDef/Hooks.java
@@ -1,2 +1,25 @@
-package stepDef;public class Hooks {
+package stepDef;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class Hooks {
+
+    @BeforeAll
+    public static void setUp(){
+    }
+
+    @AfterAll
+    public static void tearDown() {
+    }
+
+    @Before
+    public void beforeTest() {
+    }
+
+    @After
+    public void afterTest() {
+    }
 }
