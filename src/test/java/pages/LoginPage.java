eff3f598d7a87bceabab1f86c9f3b3447ff60580
dipran11 <158012785+dipran11@users.noreply.github.com>
2024-09-20 11:32:20 +0700

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,2 +1,22 @@
-package pages;public class LoginPage {
+package pages;
+
+import helper.Endpoint;
+
+public class LoginPage {
+
+    String setURL;
+
+    public String prepareURL(String url) {
+        switch (url) {
+            case "Website_souce_demo":
+                setURL = Endpoint.Website_souce_demo;
+                break;
+            default:
+                System.out.println("URL ready ");
+        }
+        System.out.println("URL ready " + setURL);
+        return setURL;
+
+
+    }
 }
